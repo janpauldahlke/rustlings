6bb6cdfbb2b42b717c97685d39d3386633941da6
HagbardCeline <janpauldahlke@gmail.com>
2022-11-28 16:04:08 +0100

    starting with errors

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -26,6 +26,8 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
+    //match qty.is_alphanumeric()
+
     Ok(qty * cost_per_item + processing_fee)
 }
 
