ff23789cb67ca05c848ad56b7243254609535a6f
HagbardCeline <janpauldahlke@gmail.com>
2023-01-03 08:33:24 +0100

    stuck on iterators 3 for now

--- exercises/standard_library_types/iterators1.rs
+++ exercises/standard_library_types/iterators1.rs
@@ -10,9 +10,7 @@
 
 fn main() {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
-
     let mut my_iterable_fav_fruits = my_fav_fruits.iter(); // TODO: Step 1
-
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"custard apple")); // TODO: Step 2
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"avocado"));
